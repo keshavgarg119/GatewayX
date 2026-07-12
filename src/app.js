979ba632625ff452afc158cdf801079f8bd90f67
Keshav Garg <kgarg2_be23@thapar.edu>
2026-07-12 09:34:28 +0530

    feat: implementation request logging middleware

--- src/app.js
+++ src/app.js
@@ -1,7 +1,13 @@
 const express = require("express");
+
+const loggerMiddleware = require("./middleware/logger.middleware");
 const healthRoutes = require("./routes/health.routes");
+
 const app = express();
 
+app.use(loggerMiddleware);
 app.use("/", healthRoutes);
 
 module.exports = app;
+
+
---
+++ src/middleware/logger.middleware.js
@@ -0,0 +1,9 @@
+const loggerMiddleware = (req, res, next) => {
+    console.log(
+        `[${new Date().toISOString()}] ${req.method} ${req.originalUrl}`
+    );
+
+    next();
+};
+
+module.exports = loggerMiddleware;
