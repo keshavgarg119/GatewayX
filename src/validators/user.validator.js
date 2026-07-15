8a5c791dd22014f84bb6faf0ca11ec582976cb30
Keshav Garg <kgarg2_be23@thapar.edu>
2026-07-15 18:15:12 +0530

    feat: improved ZOD schema

--- src/validators/user.validator.js
+++ src/validators/user.validator.js
@@ -1,9 +1,9 @@
 const { z } = require("zod");
 
 const validateUserSchema = z.object({
-    name: z.string(),
-    email: z.string(),
-    age: z.number()
+    name: z.string("Name must be a string").trim().min(2, "Name must be at least 2 characters long").max(50, "Name must be less than 50 characters"),
+    email: z.string("Email must be a string").trim().email("Invalid email address"),
+    age: z.number("Age must be a number").min(18, "Age must be at least 18 years old").max(100, "Age must be less than 100 years old")
 });
 
 module.exports = {
